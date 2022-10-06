e487b7bda3a472d2e94c06da673d5dca2481f039
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-06 11:07:02 +0200

    finished tailwind init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function App() {
+
+
+const App:React.FC = () => {
   return (
     <div className="App">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
+
+
 
 export default App;
