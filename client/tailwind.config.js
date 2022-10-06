b36bc549881e0eea7f9d6f7bb6fd81fbcabab3f9
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-06 19:03:41 +0200

    pckgs configs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}" ],
   theme: {
+    width:{
+      "full":"100vw",
+      "semi-full":"50%",
+    },
+    
+    height: {
+      "full":"95vh",
+      "semi-full" : "50%"
+    },
+
+    gap: {
+      "logo-sign":"65%"
+    },
+
     extend: {},
   },
   plugins: [],
