5d9522ea1c3a8c4c73d312e551519c189f0b79d4
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-06 21:06:19 +0200

    finished first part of homePage

--- client/src/components/getStarted.tsx
+++ client/src/components/getStarted.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 
 const GetStarted = () => {
     return (
-        <div></div>
+        <div className='w-[50vw] h-[9.5vh] flex flex-row mx-auto border-solid border border-gray-800'>
+            <input type="text" placeholder='Email Address' className=' h-[100%] bg-white w-[70%] outline-none p-5 font-sans text-xl' />
+            <button className=' h-[100%] bg-red-600 w-[30%] font-sans font-normal text-white text-3xl'>Get Started &gt;</button>
+        </div>
     );
 }
 
-
+export default GetStarted;
--- client/src/components/homeDiscription.tsx
+++ client/src/components/homeDiscription.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 const HomeDiscription:React.FC = () => {
     return (
-        <div className='text-white flex flex-col justify-center items-center mt-20'>
-            <h1 className=' text-6xl font-bold mb-3'>Unlimited movies, TV</h1>
-            <h2 className=' text-6xl font-bold mb-3'>shows, and more.</h2>
-            <p className=' text-3xl font-normal my-4'>watch anywhere. cancel anytime.</p>
-            <p className=' text-2xl font-normal mt-6'>Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className='text-white flex flex-col justify-center items-center mt-[4.5rem]'>
+            <h1 className=' text-[4rem] font-semibold'>Unlimited movies, TV</h1>
+            <h2 className=' text-6xl font-semibold mb-1'>shows, and more.</h2>
+            <p className=' text-3xl font-normal my-3'>watch anywhere. cancel anytime.</p>
+            <p className=' text-xl font-normal mt-6 mb-4'>Ready to watch? Enter your email to create or restart your membership.</p>
         </div>
     );
 }
