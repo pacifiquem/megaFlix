887357f60d2e54ebddc06d5f536fe294d22e6845
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-06 21:05:58 +0200

    components for showercasing functionalities

---
+++ client/src/components/functionalities.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+export const EnjoyOnYourTv:React.FC = () => {
+    return (
+        <></>
+    );
+}
+
+
+
+
+
+export const DownloadToWatchOffline:React.FC = () => {
+    return (
+        <></>
+    );
+}
+
+
+
+
+
+export const WatchEveryWhere:React.FC = () => {
+    return (
+        <></>
+    );
+}
+
+
+
+
+export const ProfileForKids:React.FC = () => {
+    return (
+        <></>
+    )
+}
