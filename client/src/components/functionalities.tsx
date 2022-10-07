53a31f00070c3f3d2c3b6bdf24593671eb3e287d
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-07 10:19:36 +0200

    add functionality components (s)

--- client/src/components/functionalities.tsx
+++ client/src/components/functionalities.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-
+import tvImage from '../assets/images/tvImage.png';
 
 export const EnjoyOnYourTv:React.FC = () => {
     return (
-        <div className="w-[100vw] h-[55vh] border-b-8 border-[#272525] flex flex-row">
-            <div className=' w-[50%] h-[100%] bg-red-500 flex flex-col items-center'>
-                <h1>Enjoy on your TV.</h1>
-                <h2>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h2>
+        <div className="w-[100vw] h-[60vh] border-b-8 border-[#272525] flex flex-row">
+            <div className=' w-[50%] h-[100%] flex justify-center items-center'>
+                <div className=" text-white w-[60%]">
+                    <h1 className="mb-8 text-5xl font-semibold">Enjoy on your TV.</h1>
+                    <h2 className="text-2xl font-normal my-3">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h2>
+                </div>
             </div>
-            <div className=" w-[50%] h-[100%] bg-slate-200"></div>
+            <div className=" w-[50%] h-[100%] flex justify-center mt-5">
+                <div className=' w-[60%] h-[80%] relative'>
+                    <img src={tvImage} alt="tvImage" className="absolute z-10 w-[20vh] h-[100%]"/>
+                    <video muted loop autoPlay className="w-[100%] h-[110%]">
+                        <source src="http://127.0.0.1:5500/client/src/assets/images/tvVideo.m4v"/>
+                    </video>
+                </div>
+            </div>
         </div>
     );
 }
@@ -19,7 +28,18 @@ export const EnjoyOnYourTv:React.FC = () => {
 
 export const DownloadToWatchOffline:React.FC = () => {
     return (
-        <></>
+        <div className="w-[100vw] h-[60vh] border-b-8 border-[#272525] flex flex-row">
+            <div className=" w-[50%] h-[100%] flex justify-center">
+                <div className=' w-[60%] h-[80%] mt-8 relative bg-slate-500'>
+                </div>
+            </div>
+            <div className=' w-[50%] h-[100%] flex items-center'>
+                <div className=" text-white w-[80%] ">
+                    <h1 className="mb-8 text-5xl font-semibold">Download your shows to watch offline.</h1>
+                    <h2 className="text-2xl font-normal my-3">Save your favorites easily and always have something to watch.</h2>
+                </div>
+            </div>
+        </div>
     );
 }
 
@@ -29,7 +49,12 @@ export const DownloadToWatchOffline:React.FC = () => {
 
 export const WatchEveryWhere:React.FC = () => {
     return (
-        <></>
+        <div className="w-[100vw] h-[35vh] border-b-8 border-[#272525] flex items-center">
+            <div className=" text-white pl-40 w-[45%]">
+                <h1 className="mb-8 text-5xl font-semibold">Watch everywhere.</h1>
+                <h2 className="text-2xl font-normal my-3">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</h2>
+            </div>
+        </div>
     );
 }
 
@@ -38,6 +63,17 @@ export const WatchEveryWhere:React.FC = () => {
 
 export const ProfileForKids:React.FC = () => {
     return (
-        <></>
+        <div className="w-[100vw] h-[60vh] border-b-8 border-[#272525] flex flex-row">
+            <div className=" w-[50%] h-[100%] flex justify-center">
+                <div className=' w-[60%] h-[80%] mt-8 relative bg-slate-500'>
+                </div>
+            </div>
+            <div className=' w-[50%] h-[100%] flex items-center'>
+                <div className=" text-white w-[60%] ">
+                    <h1 className="mb-8 text-5xl font-semibold inline-block w-[120%]">Create profiles for kids.</h1>
+                    <h2 className="text-2xl font-normal my-3">Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</h2>
+                </div>
+            </div>
+        </div>
     )
 }
