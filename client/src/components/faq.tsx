9886e2a144ac8f132b82e2313b29e0ea222417f6
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-07 10:19:01 +0200

    added FAQ component

---
+++ client/src/components/faq.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+const Faq:React.FC = () => {
+    return (
+        <div>
+            <h1 className='text-white text-center my-24 text-5xl font-semibold'>Frequently Asked Questions</h1>
+            <div className=' w-[55vw] m-auto h-[40vh]'>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+                <div className='flex flex-col mb-8'>
+                    <div className=' bg-gray-500 text-white'>Welcome</div>
+                    <div className=' bg-gray-500'></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Faq;
