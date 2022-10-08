0a20c43e6490e7e2eb2e824d86502b4a2cf302a4
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-08 13:36:30 +0200

    home part 1

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -9,7 +9,7 @@ import Faq from '../components/faq';
 
 const Home:React.FC = () => {
     return (
-        <div className=' overflow-hidden'>
+        <>
             <div className="container bg-cover w-full h-full bg-repeat border-b-8 border-[#272525]" style={{backgroundImage : `linear-gradient(to top,rgba(0,0,0,.9)0, rgba(0,0,0,.3)60%, rgba(0,0,0,.3)70%, rgba(0,0,0,.9)100%), url(${background})`}}>
                 <div className=" flex flex-row gap-logo-sign py-9">
                     <img src={logo} alt="Megaflix logo" className=' mx-14 hover:cursor-pointer' />
@@ -23,7 +23,7 @@ const Home:React.FC = () => {
             <WatchEveryWhere />
             <ProfileForKids />
             <Faq />
-        </div>
+        </>
     );
 }
 
