70f49f8dbcdab3c5fbf6bc0c337e7d0a21478ae6
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-08 15:14:31 +0200

    finished footer.tsx

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className='h-[40vh] w-full flex flex-row justify-center items-center'>
+            <div className='flex flex-col items-start gap-[5%] text-[#4e4e4e] w-[20%] h-[80%]'>
+                <p className=' hover:underline cursor-pointer text-lg font-medium'>Question ? Contact us</p>
+                <p className=' hover:underline cursor-pointer mt-3'>FAQ</p>
+                <p className=' hover:underline cursor-pointer'>Investor Relations</p>
+                <p className=' hover:underline cursor-pointer'>Privacy</p>
+                <p className=' hover:underline cursor-pointer'>Speed Test</p>
+                <p className=' hover:underline cursor-pointer'>MegaFlix Rwanda</p>
+            </div>
+            <div className='flex flex-col items-start gap-[5%] text-[#4e4e4e] w-[20%] h-[80%]'>
+                <p className=' hover:underline cursor-pointer mt-14'>Help Center</p>
+                <p className=' hover:underline cursor-pointer'>Jobs</p>
+                <p className=' hover:underline cursor-pointer'>Cookie Preferences</p>
+                <p className=' hover:underline cursor-pointer'>Legal Notices</p>
+            </div>
+            <div className='flex flex-col items-start gap-[5%] text-[#4e4e4e] w-[20%] h-[80%]'>
+                <p className=' hover:underline cursor-pointer mt-14'>Account</p>
+                <p className=' hover:underline cursor-pointer'>Ways to Watch</p>
+                <p className=' hover:underline cursor-pointer'>Corporate Information</p>
+                <p className=' hover:underline cursor-pointer'>Only on MegaFlix</p>
+            </div>
+            <div className='flex flex-col items-start gap-[5%] text-[#4e4e4e] w-[20%] h-[80%]'>
+                <p className=' hover:underline cursor-pointer mt-14'>Media Center</p>
+                <p className=' hover:underline cursor-pointer'>Terms of Use</p>
+                <p className=' hover:underline cursor-pointer'>Contact Us</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
