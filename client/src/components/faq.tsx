a57c8dd6fb88c304103236cdd6189bb703f0ffdc
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-08 10:54:57 +0200

    firstpart of faq finished

--- client/src/components/faq.tsx
+++ client/src/components/faq.tsx
@@ -4,36 +4,57 @@ import React from 'react';
 const Faq:React.FC = () => {
     return (
         <div>
-            <h1 className='text-white text-center my-24 text-5xl font-semibold'>Frequently Asked Questions</h1>
-            <div className=' w-[55vw] m-auto h-[40vh]'>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+            <h1 className='text-white my-16 font-semibold text-5xl text-center'>Frequently Asked Questions</h1>
+            <div className='w-[55vw] mx-auto'>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>What is Netflix?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!</h1>
+                    </div>
                 </div>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>How much does Netflix cost?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>Watch Metaflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from US$3.99 to US$11.99 a month. No extra costs, no contracts.</h1>
+                    </div>
                 </div>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>Where can I watch?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>
+                            Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+                            You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+                        </h1>
+                    </div>
                 </div>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>How do I cancel?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>
+                            Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
+                        </h1>
+                    </div>
                 </div>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>What can I watch on Netflix?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>
+                        Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
+                        </h1>
+                    </div>
                 </div>
-                <div className='flex flex-col mb-8'>
-                    <div className=' bg-gray-500 text-white'>Welcome</div>
-                    <div className=' bg-gray-500'></div>
+                <div className='w-[100%] mb-2'>
+                    <button className='w-[100%] text-white text-3xl font-normal text-left py-6 px-12 bg-[#363636] mb-[2px]'>Is Netflix good for kids?</button>
+                    <div className='w-[100%] p-9 font-normal text-2xl  bg-[#363636] text-white hidden'>
+                        <h1 className='text-start'>
+                            The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.
+                            Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.
+                        </h1>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
 
 export default Faq;
