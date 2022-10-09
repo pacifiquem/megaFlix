2dbe42f0635c3edea0df2b2dc3be7320a204fac7
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-09 08:23:29 +0200

    changed name from home to welcome

---
+++ client/src/pages/welcome.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import background from '../assets/images/background.png';
+import logo from '../assets/images/logo.png';
+import GetStarted from '../components/getStarted';
+import HomeDiscription from '../components/homeDiscription';
+import { EnjoyOnYourTv, DownloadToWatchOffline, WatchEveryWhere, ProfileForKids } from '../components/functionalities';
+import Faq from '../components/faq';
+import Footer from '../components/footer';
+
+
+const Welcome:React.FC = () => {
+    return (
+        <>
+            <div className="container bg-cover w-full h-full bg-repeat border-b-8 border-[#272525]" style={{backgroundImage : `linear-gradient(to top,rgba(0,0,0,.9)0, rgba(0,0,0,.3)60%, rgba(0,0,0,.3)70%, rgba(0,0,0,.9)100%), url(${background})`}}>
+                <div className=" flex flex-row gap-logo-sign py-9">
+                    <img src={logo} alt="Megaflix logo" className=' mx-14 hover:cursor-pointer' />
+                    <button className=' bg-red-600 p-0 text-white font-semibold text-lg rounded w-[6%] h-[5vh]'>Sign In</button>
+                </div>
+                <HomeDiscription />
+                <GetStarted />
+            </div>
+            <EnjoyOnYourTv />
+            <DownloadToWatchOffline />
+            <WatchEveryWhere />
+            <ProfileForKids />
+            <Faq />
+            <Footer />
+        </>
+    );
+}
+
+export default Welcome;
