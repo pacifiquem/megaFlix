2570fd39c17c131663bac84d79c12a54a0a72d88
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-10 08:53:58 +0200

    defaultMovie.tsx added

---
+++ client/src/components/defaultMovie.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BsInfoCircle } from 'react-icons/bs';
+import { FaPlay } from 'react-icons/fa'
+
+
+interface props {
+    name: string,
+    discription: string
+}
+
+const DefaultMovie:React.FC <props> = (props) => {
+    return (
+        <div className='w-[60%] h-[50%] p-16 text-white'>
+            <h1 className='text-[4rem] font-semibold pt-16'>{props.name}</h1>
+            <div  className='w-[70%] mb-8'>
+                <p className='text-xl font-normal mt-6 mb-4'>{props.discription}</p>
+            </div>
+            <button className='w-[20%] bg-[#4c484877] hover:bg-[#b3b3b386] px-2 py-3 text-2xl rounded'><FaPlay className='inline mr-2'/> Play</button>
+            <button className='w-[25%] bg-[#b3b3b386] hover:bg-[#4c484877] px-2 py-3 text-2xl ml-8 rounded'><BsInfoCircle className='inline mr-2'/> More Info </button>
+        </div>
+    );
+}
+
+export default DefaultMovie;
--- client/src/components/getStarted.tsx
+++ client/src/components/getStarted.tsx
@@ -5,7 +5,7 @@ const GetStarted:React.FC = () => {
     return (
         <div className='flex flex-col justify-center items-center'>
             <p className=' text-xl font-normal mt-6 mb-4 text-white'>Ready to watch? Enter your email to create or restart your membership.</p>
-            <div className='w-[50vw] h-[9.5vh] flex flex-row mx-auto border-solid border border-gray-800'>
+            <div className='w-[50vw] h-[9.5vh] flex flex-row mx-auto border-solid border border-gray-800 rounded'>
                 <input type="text" placeholder='Email Address' className=' h-[100%] bg-white w-[70%] outline-none p-5 font-sans text-xl' />
                 <button className=' h-[100%] bg-red-600 w-[30%] font-sans font-normal text-white text-3xl'>Get Started &gt;</button>
             </div>
