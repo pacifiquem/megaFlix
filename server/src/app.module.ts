24bb336668cf4b30f3ccfb9e1a23680b00f09025
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-11 09:13:21 +0200

    *[semi-fin] : *

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
 import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, MoviesController, UsersController],
-  providers: [AppService, MoviesService],
+  controllers: [AppController, UsersController],
+  providers: [AppService],
 })
 export class AppModule {}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Put, Delete, Req, Res } from '@nestjs/common';
+
+import { Request, Response } from 'express';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Post()
+    async newUser(){
+        return 'all is well';
+    }
+}
