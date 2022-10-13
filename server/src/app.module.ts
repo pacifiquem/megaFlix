c01840069f1a34c3e43099a0f4600f5e68b5b587
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-13 19:13:28 +0200

    [refrector] : removed unused vars

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
