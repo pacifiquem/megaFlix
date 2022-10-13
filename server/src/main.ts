8be8cdf19bb95b747f4c979c5357754f1d550523
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-13 06:28:37 +0200

    *[new] : mongo-sanitize

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import * as mongoSanitize from 'express-mongo-sanitize';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.use(helmet());
+  app.use(mongoSanitize());
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000, () => {
     console.log(`nest server is running at ${process.env.PORT}`);
   });
