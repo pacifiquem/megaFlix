b5f4282f0f4dc3acd1a56d64e41f9dfd422223ef
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-13 21:26:37 +0200

    [finish] finished backend API

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    LoggerModule.forRoot(),
     MongooseModule.forRoot(`${process.env.DB_URL}`),
     UsersModule,
   ],
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,14 +3,19 @@ import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import * as mongoSanitize from 'express-mongo-sanitize';
+import { Logger } from 'nestjs-pino';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+async function megaflixServerServicesProvider() {
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'https://atmegaflix.vercel.app' },
+    bufferLogs: true,
+  });
   app.use(cookieParser());
   app.use(helmet());
+  app.useLogger(app.get(Logger));
   app.use(mongoSanitize());
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000, () => {
     console.log(`nest server is running at ${process.env.PORT}`);
   });
 }
-bootstrap();
+megaflixServerServicesProvider();
