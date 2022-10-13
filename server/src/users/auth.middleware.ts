8fcdb48edc931b603dc1d1d3d16d9e8bf1135c39
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-13 19:09:07 +0200

    [feat] authentication middleware added

---
+++ server/src/users/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Response, NextFunction } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import { jwtDTO } from './dtos/jwt.dot';
+import jwtFunctions from './utils/jwt_token_generator.util';
+
+@Injectable()
+export class authMiddleware implements NestMiddleware {
+  use(req: jwtDTO, res: Response, next: NextFunction) {
+    if (req.cookies.token) {
+      const token = req.cookies.token;
+      // eslint-disable-next-line prettier/prettier
+      const tokensCredential: JwtPayload | string =
+        jwtFunctions.jwtValidator(token);
+      req.user = tokensCredential;
+
+      next();
+    } else {
+      res.json({
+        successful: false,
+        message: 'you are not authorized to access this route',
+      });
+    }
+  }
+}
