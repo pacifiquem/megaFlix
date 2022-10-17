b9844e4b6ffd84b4997ef250cc0b1f589b2eb246
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-17 09:21:47 +0200

    [feat] fetchMovie added

--- client/src/utils/fetchMovies.ts
+++ client/src/utils/fetchMovies.ts
@@ -1,23 +1,21 @@
-import dotenv from 'dotenv';
-import path from 'path';
-
-const pathToEnv:string = path.join(__dirname, '..', '/config/.env');
-console.log(pathToEnv);
-dotenv.config({path: pathToEnv});
+import urlInfo from "./aboutMovies";
 
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = urlInfo.API_KEY;
+const BASE_URL = urlInfo.BASE_URL;
 
 
-const requests = {
-  fetchTrending: `${process.env.BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `${process.env.BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
-  fetchComedyMovies: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
-  fetchHorrorMovies: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
-  fetchRomanceMovies: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
-  fetchDocumentaries: `${process.env.BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
+const  requests = {
+  fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
+  fetchComedyMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
+  fetchHorrorMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
+  fetchRomanceMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
+  fetchDocumentaries: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
 }
+
+export const MovieRequests = [ requests.fetchTopRated, requests.fetchTrending ]
 
 export default requests;
