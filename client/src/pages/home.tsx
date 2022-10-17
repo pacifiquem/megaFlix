0cd14f290479b3534f5a1c75a40ebabdd91911dc
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-17 09:22:52 +0200

    defaultMovie added to homepage

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import Nav from '../components/nav';
 import DefaultMovie from '../components/defaultMovie';
 
+
 const Home:React.FC = () => {
+
     return (
         <div>
             <Nav />
-            <DefaultMovie name='pac' discription='While hanging out after school, Charlie and his friends discover the headquarters of the world’s most powerful superhero hidden beneath his home. When villains attack, they must team up to defend the headquarters and save the world.' />
+            <DefaultMovie />
         </div>
     );
 }
