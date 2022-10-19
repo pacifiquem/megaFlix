3a9cd1970ae088dfade06e9aeaa7522e8b32b7e1
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-19 05:48:46 +0200

    [start] :  dai

--- client/src/components/listOfMovies.tsx
+++ client/src/components/listOfMovies.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React,  { useState } from 'react';
+import aboutMovies from '../utils/aboutMovies';
 
 interface props {
     imageUrl: string,
@@ -7,7 +7,10 @@ interface props {
 }
 
 const ListOfMovies:React.FC <props> = (props) => {
-    console.log(props);
+
+    const [movie_id, setMovie_id] = useState(0);
+    const [movieUrl, setMovieUrl] = useState(`https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${aboutMovies.API_KEY}&language=en-US`);
+
     return (
         <div className='w-[20%] h-[20vh] hover:w-[21%] hover:h-[21.5vh] hover:cursor-pointer rounded' style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${props.imageUrl})`, backgroundSize: `100% 100%`}}>
         </div>
