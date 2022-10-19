486545046fef63d3b73684ac2b6f47f39a83e1c1
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-19 11:36:02 +0200

    [feat] : watcMovie component added

---
+++ client/src/components/watchMovie.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface idType{
+    id:number
+}
+
+const WatchMovie = (watch: idType) => {
+    return (
+        <div className='absolute'>
+
+        </div>
+    )
+}
+
+export default WatchMovie;
