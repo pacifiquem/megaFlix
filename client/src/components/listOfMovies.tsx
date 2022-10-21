3839bf7c38fd42e3017d6956a49a07ec763f32f6
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-21 07:57:58 +0200

    [feat] : integrated with watchMovie Page

--- client/src/components/listOfMovies.tsx
+++ client/src/components/listOfMovies.tsx
@@ -1,43 +1,34 @@
 import axios from 'axios';
-import React,  { useState } from 'react';
+import React, { useState } from 'react';
+import {
+    Routes,
+    Route,
+    Link,
+    Navigate,
+  } from 'react-router-dom';
 import aboutMovies from '../utils/aboutMovies';
-import WatchMovie from './watchMovie';
 
 interface props {
     imageUrl: string,
     id:Number,
+    type:number[]
 }
 
 const ListOfMovies:React.FC <props> = (props) => {
 
-    const [movie_id, setMovie_id] = useState('');
-    const [movie_title, setMovie_title] = useState('');
-    const [showMovie, setShowMovie] = useState(false);
-    const [movieUrl] = useState(`https://api.themoviedb.org/3/movie/${props.id}/videos?api_key=${aboutMovies.API_KEY}&language=en-US`);
+    const [navigate, setNavigate] = useState(false);
+    const [url] = useState(`/watch?id=${props.id}&genre=${props.type[0]}`);
 
-    const FetchMovie = () => {
-
-        axios.get(movieUrl).then((response) => {
-            response.data.results.map((video: any) => {
-                if(video.type === "Trailer") {
-                    setMovie_id(video.key);
-                    setMovie_title(video.name);
-                    setShowMovie(!showMovie);
-                }
-            })
-        }).catch((error) => {
-            console.log(error);
-        });
-
+    const sendToWatchMovie = () => {
+        setNavigate(!navigate);
     }
 
-
     return (
-        <div className='w-[20%] h-[20vh] hover:w-[21%] hover:h-[21.5vh] hover:cursor-pointer rounded' onClick={ FetchMovie } style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${props.imageUrl})`, backgroundSize: `100% 100%`}}>
-            {
-                <WatchMovie trailerUrl={movie_id} title={movie_title} /> && showMovie
-            }
-        </div>
+        <>
+            <div className='w-[20%] h-[20vh] hover:w-[21%] hover:h-[21.5vh] hover:cursor-pointer rounded' onClick={sendToWatchMovie} style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${props.imageUrl})`, backgroundSize: `100% 100%`}}>
+            </div>
+            { navigate && <Navigate replace to={url} /> }
+        </>
     );
 }
 
