9629e79bb4b3d8fb96d04d5b81122febaf981031
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-21 21:44:36 +0200

    [end] : next dai

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -11,14 +11,28 @@ import Copyright from '../components/copyright';
 
 const Signup:React.FC = () => {
 
+    const header = {
+        'Access-Control-Allow-Origin' : 'atmegaflix.vercel.app'
+    }
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const formHandler = () => {
-        // if(username !== null && email !== null && password !== null) {
-        //     axios.post()
-        // }
+    const formHandler = (event:any) => {
+        event.preventDefault();
+        if(username !== null && email !== null && password !== null) {
+            axios.post('https://atmegaflix.herokuapp.com/user/signup' , {
+                username,
+                email, 
+                password
+            },{
+                headers:header
+            }).then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
     }
 
     return (
@@ -26,7 +40,7 @@ const Signup:React.FC = () => {
             <img src={logo} alt="Megaflix logo" />
             <div className='w-[32%] h-[85%] bg-[#000000c5] mx-auto mt-6 mb-4 py-14 px-14'>
                 <h1 className='text-start text-white text-4xl font-semibold mb-12'>Sign Up</h1>
-                <form method="post" className='w-[100%] h-[75%]'>
+                <form onSubmit={formHandler} className='w-[100%] h-[75%]'>
                     <div className='w-[100%] h-[13%] flex flex-row bg-red-600 mb-6 rounded'>        
                         <FiUser className='w-[15%] h-[60%] mt-3' />
                         <input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} placeholder='Username' required className='w-[85%] h-[100%] outline-none px-8 rounded-r'/>
@@ -37,9 +51,9 @@ const Signup:React.FC = () => {
                     </div>
                     <div className='w-[100%] h-[13%] flex flex-row bg-red-600 mb-6 rounded'>     
                         <BiLock className='w-[15%] h-[60%]  mt-3' />
-                        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder='Password' required className='w-[85%] h-[100%] outline-none px-8 rounded-r'/>
+                        <input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder='Password' required autoComplete='true' className='w-[85%] h-[100%] outline-none px-8 rounded-r'/>
                     </div>
-                    <button type='submit' className=' bg-red-600 w-[100%] h-[15%] font-sans font-semibold text-white text-2xl rounded' onClick={formHandler}>Sign Up</button>
+                    <button type='submit' className=' bg-red-600 w-[100%] h-[15%] font-sans font-semibold text-white text-2xl rounded'>Sign Up</button>
                     <p className='text-[#4e4e4e] mt-12 text-lg'>not New To Netflix ? <span className='text-white hover:cursor-pointer'>Sign In</span></p>
                     <p className='text-[#4e4e4e] mt-2'>This page is protected by reCAPTCHA to ensure you are not a bot .</p>
                 </form>
