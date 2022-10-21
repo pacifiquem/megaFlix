7e5cf18f04e757bec05cc5ac9421d074bfa51b25
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-21 07:56:33 +0200

    [feat] : watch route adde

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,18 @@ import Home from './pages/home';
 import Welcome from './pages/welcome';
 import Signup from './pages/signup';
 import SignIn from './pages/signin';
+import WatchMovie from './pages/watchMoviePage';
 
 const App:React.FC = () => {
   return (
     <div className="App overflow-x-hidden">
       <BrowserRouter> 
             <Routes>
-              <Route path='/' index element={<Home />} />
-              <Route path='/welcome' element={<Welcome />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/' index element={<Welcome />} />
               <Route path='/signup' element={<Signup />} />
               <Route path='/signin' element={<SignIn />} />
+              <Route path="/watch" element={<WatchMovie />} />
             </Routes>
       </BrowserRouter>
     </div>
