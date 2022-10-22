dc316c4735d97f331f9fc7bbc5722bf2db3c2b4b
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-22 13:11:43 +0200

    [feat] : set liimits

--- server/src/users/schemas/users.schema.ts
+++ server/src/users/schemas/users.schema.ts
@@ -2,23 +2,25 @@
 import * as mongoose from 'mongoose';
 
 export const UsersSchema = new mongoose.Schema({
+  
+  password: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+
   username: {
     type: String,
     required: true,
     unique: false,
-    maxLength: 20,
-    minLength: 4,
+    maxLength: [15, "too long username"],
+    minLength: [3, "too short username"],
   },
 
   email: {
     type: String,
     required: true,
     unique: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
   },
 
   resetPasswordToken: {
