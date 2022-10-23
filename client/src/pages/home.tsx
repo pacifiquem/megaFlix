9fc1c979a54b0156e1333ffce1d6434a800e3e2e
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-23 21:29:46 +0200

    [feat] : cookie-management integration-to-backend

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,19 +1,32 @@
+import React, { useState } from 'react';
 import Nav from '../components/nav';
 import DefaultMovie from '../components/defaultMovie';
 import Movies from '../components/movies';
+import { Cookies } from 'react-cookie';
+import { Navigate } from 'react-router-dom';
 
 
 const Home:React.FC = () => {
 
+    const [cookie, setCookie] = useState(new Cookies());
+    const [sendToLogin, setSendToLogin] = useState(false);
+
+    const isLoggedIn = () => {
+        if(cookie.get('token') === null || cookie.get('token') === undefined) {
+            setSendToLogin(!sendToLogin);
+        }else {
+            return;
+        }
+    }
+
     return (
-        <div>
+        <div onLoad={isLoggedIn}>
             <Nav />
             <DefaultMovie />
             <Movies />
+            { sendToLogin && <Navigate replace to='/signin' /> }
         </div>
     );
 }
-
-
 
 export default Home;
