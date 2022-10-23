57983fdbf762fe87751a91dc7fed56359cbaa505
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-23 21:31:13 +0200

    cookie-management

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CookiesProvider from 'react-cookie/cjs/CookiesProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CookiesProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </CookiesProvider>
 );
 reportWebVitals();
