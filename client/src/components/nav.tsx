37a504eea2d6998a9c57f9ce02cb0d83be100a52
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-24 09:30:12 +0200

    [feat] : changeColor onscroll

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {BiSearch, BiNotification} from 'react-icons/bi'
 import logo from '../assets/images/logo.png';
 
 
 const Nav:React.FC = () => {
+    const navDivRef = useRef<HTMLDivElement>(null);
+    const [windowscrollY] = useState(window.scrollY);
+
+    const changeBackground = () => {
+        if (window.scrollY !== 0) {
+            navDivRef.current?.classList.replace('bg-[#ffffff00]','bg-[rgba(0,0,0,0.6)]');
+        } else {
+            navDivRef.current?.classList.replace('bg-[rgba(0,0,0,0.6)]','bg-[#ffffff00]');
+        }
+      }
+
+    useEffect(() => {
+        changeBackground();
+        window.addEventListener('scroll', changeBackground);
+    }, [windowscrollY]);
+
     return(
-        <div className="flex flex-row gap-[3%] h-[8vh] px-16 justify-start items-center bg-[#ffffff00] sticky top-0 z-10">
+        <div ref={navDivRef} className="flex flex-row gap-[3%] h-[8vh] w-[100vw] px-16 justify-start items-center bg-[#ffffff00] fixed z-10 top-0">
              <img src={logo} alt="Megaflix logo" className='hover:cursor-pointer w-[8%] h-[70%]' onClick={() => {window.location.href='https://atmegaflix.vercel.app/welcome'}}/>
              <div className="text-white flex flex-row gap-[4%] w-[40%] font-medium text-xl">
                 <div className="flex justify-center items-center hover:cursor-pointer hover:text-red-500 hover:underline">Home</div>
