e0142a6ade97994f39c292e21b2fd3c056134aea
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-23 09:17:35 +0200

    feat: added types
    Co-authored-by: ndzhwr ndizihiweregis06@gmail.com

--- client/src/components/homeDiscription.tsx
+++ client/src/components/homeDiscription.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import {FC} from 'react'
 
-
-const HomeDiscription:React.FC = () => {
+const HomeDiscription:FC = () => {
     return (
         <div className='text-white flex flex-col justify-center items-center mt-[4.5rem]'>
             <h1 className=' text-[3.5rem] tracking-tight md:tracking-normal md:text-[4rem] font-semibold'>Unlimited movies, TV</h1>
